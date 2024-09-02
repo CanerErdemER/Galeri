e6272d49396992d1fc1f9ae7ba82c017d9b4f096
caner erdem er <102194455+CanerErdemER@users.noreply.github.com>
2024-09-02 13:46:49 +0300

    ext2

--- Galeri/views.py
+++ Galeri/views.py
@@ -5,8 +5,10 @@ from .forms import ArabalarForm
 
 # Create your views here.
 
-
-    
+def index(request):
+    sayfa='index.html'
+    ctx={'arabalar':Arabalar.objects.all()}  
+    return render(request,sayfa,ctx)
 
 def detaylar(request,slug):
     sayfa='aracdetay.html'
@@ -14,15 +16,12 @@ def detaylar(request,slug):
     
     return render(request,sayfa,ctx)
 
-def arac_Cek(request):
-
-    sayfa='index.html'
-    ctx={'arabalar':Arabalar.objects.all()}  
-    
+def edit(request,slug):
     if request.method == "POST":
         form = ArabalarForm(request.POST)
         if form.is_valid():
             # # Plaka Marka_Model Fiyat modelsten geliyor
+            
             plaka=form.cleaned_data["plaka"]   
             model=form.cleaned_data["model"] 
             fiyat=form.cleaned_data["fiyat"]
@@ -30,23 +29,10 @@ def arac_Cek(request):
 
             ahmet=Arabalar(Plaka=plaka,Marka_Model=model,Fiyat=fiyat)
             ahmet.save()
-            return redirect("arac_form", )                      
+            return redirect("arac_form")                      
     else:
-        # if arac_id:
-        #     arac=Arabalar.objects.get(id=arac_id)
-        #     form=ArabalarForm(initial={
-        #         "plaka":arac.Plaka,
-        #         "model":arac.Marka_Model,
-        #         "plaka":arac.Fiyat,
-
-        #     })
-        # else:
-            form=ArabalarForm()
-    return render(request, "index.html", {"form": form,'arabalar':Arabalar.objects.all()})
-
-
-
-
+        form = ArabalarForm()
+    return render(request, "aracduzenle.html", {"form": form,'arabalar':Arabalar.objects.all()})
 
 
 
--- main/urls.py
+++ main/urls.py
@@ -5,13 +5,14 @@ from Galeri import views
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    
-    path("",views.arac_Cek,name="arac_form"),
-   
-    path("<int:arac_id>",views.arac_Cek,name="arac_form"),
+
+    path("",views.index,name="arac_form"),
     
     path("arac/<slug:slug>",views.detaylar,name="arac_detay"),
+
+    path("arac/<slug:slug>/edit",views.edit,name="arac_duzenle"),
     
     
       
